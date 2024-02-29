980735d4d51f0e3e1a050b6d7d1ba605206af80f
jmshtr <157889640+jmshtr@users.noreply.github.com>
2024-02-29 11:20:44 +0000

    Update default.nix

--- home/default.nix
+++ home/default.nix
@@ -22,14 +22,19 @@
   programs.bash = {
     enable = true;
     enableCompletion = true;
-    initExtra = ''. "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"'';
-    bashrcExtra = ''export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"'';
+    initExtra = ''
+      source "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
+    '';
+    bashrcExtra = ''
+      export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
+    '';
     profileExtra = ''
-    . "/home/james/.sh/mount_nextcloud.sh"
-    export XDG_DATA_DIRS=$XDG_DATA_DIRS:/usr/share:/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share
+      . "/home/james/.sh/mount_nextcloud.sh"
+      export XDG_DATA_DIRS=$XDG_DATA_DIRS:/usr/share:/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share
     '';
     shellAliases = {};
   };
+
 
   home.sessionVariables = {
     # EDITOR = "emacs";
